f041f2ac38a269efbf5d04e0b152a31cf2a33439
Ethanscharlie <ethanscharlie@protonmail.com>
2025-05-09 13:51:26 -0600

    Button pressed from controller

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,37 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_events.h>
+#include <SDL3/SDL_joystick.h>
+#include <csignal>
+#include <cstdlib>
 #include <iostream>
+#include <stdexcept>
 
-int main() { std::cout << "Hello World\n"; }
+int main() {
+  if (!SDL_Init(SDL_INIT_JOYSTICK)) {
+    std::cout << "Error: " << SDL_GetError() << "\n";
+  }
+
+  if (!SDL_HasJoystick()) {
+    throw std::logic_error("Joystick not connected");
+  }
+
+  SDL_JoystickID joystickID = SDL_GetJoysticks(NULL)[0];
+  SDL_Joystick *joystick = SDL_OpenJoystick(joystickID);
+
+  while (true) {
+    SDL_Event e;
+    while (SDL_PollEvent(&e)) {
+      switch (e.type) {
+      case SDL_EVENT_JOYSTICK_BUTTON_DOWN:
+        switch (e.jbutton.button) {
+        case 0:
+          std::cout << "Button 0 pressed\n";
+          break;
+        }
+        break;
+      }
+    }
+
+    signal(SIGINT, [](int sig) { std::exit(1); });
+  }
+}
